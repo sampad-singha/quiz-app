905b70555c0e025bdeceeb3d35337059f9d015e3
sampad201199 <sampadsingha8@gmail.com>
2023-12-24 13:34:16 +0600

    changed url

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
         }
     }
     async function questionArray(){
-        const response = await fetch("https://opentdb.com/api.php?amount=5&type=multiple");
+        const response = await fetch("https://opentdb.com/api.php?amount=5&category=18&type=multiple");
         const data = await response.json();
         const questions = data.results.map(result => {
             return buildQuestion(result)
